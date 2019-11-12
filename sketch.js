6f9da3fc207067b2f02066f9cdad031313203e6d
victoriesinarow <57379431+victoriesinarow@users.noreply.github.com>
2019-11-12 11:09:45 +0900

    p5play_spritecreation

--- sketch.js
+++ sketch.js
@@ -6,9 +6,22 @@ function setup() {
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.position(windowWidth/2 - canvasWidth/2, 20);
   noCursor();
+  drawSprites();
 }
 
 function draw() {
-  background(30);
+  background(50);
   rect(mouseX, mouseY, 100, 100);
+}
+function mousePressed() {
+
+  //create a sprite at the mouse position and store it in a temporary variable
+  var s = createSprite(mouseX, mouseY, 30, 30);
+  //if no image or animation is associated it will be a rectancle of the specified size
+  //and a random color
+
+  //now you can use the variable to set properties
+  //e.g. a random velocity on the x and y coordinates
+  s.velocity.x = random(-5, 5);
+  s.velocity.y = random(-5, 5);
 }
