f27c9ac95b290a97df0b8f978c093203f3fe5b3f
victoriesinarow <57379431+victoriesinarow@users.noreply.github.com>
2019-11-16 14:34:41 +0900

    openfile

--- sketch.js
+++ sketch.js
@@ -1,14 +1,30 @@
-let canvas;
-let canvasWidth = 600;
-let canvasHeight = 400;
+//Sprite creation
+//Click to create a new sprite with random speed
 
 function setup() {
-  canvas = createCanvas(canvasWidth, canvasHeight);
-  canvas.position(windowWidth/2 - canvasWidth/2, 20);
-  noCursor();
+  createCanvas(800, 400);
 }
 
 function draw() {
-  background(30);
-  rect(mouseX, mouseY, 100, 100);
+  background(random(), random(), random());
+
+  fill(0);
+  textAlign(CENTER);
+  text('Click to create a new sprite, move mouse to hit the wall', width/2, height/2);
+  //draw all the sprites added to the sketch so far
+  //the positions will be updated automatically at every cycle
+  drawSprites();
+}
+
+function mousePressed() {
+
+  //create a sprite at the mouse position and store it in a temporary variable
+  var s = createSprite(mouseX, mouseY, 30, 30);
+  //if no image or animation is associated it will be a rectancle of the specified size
+  //and a random color
+
+  //now you can use the variable to set properties
+  //e.g. a random velocity on the x and y coordinates
+  s.velocity.x = random(-5, 5);
+  s.velocity.y = random(-5, 5);
 }
