b18e561aa095bca94ebbecb3bda999cb656c3429
victoriesinarow <57379431+victoriesinarow@users.noreply.github.com>
2019-11-16 19:14:03 +0900

    bounce4

--- sketch.js
+++ sketch.js
@@ -5,12 +5,24 @@ let canvas;
 let canvasWidth = 800;
 let canvasHeight = 400;
 
-
+var things;
+var leftWall;
+var rightWall;
+var topWall;
 
 function setup() {
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.position(windowWidth/2 - canvasWidth/2, 20);
   noCursor(); // no show cursor
+    
+  leftWall = createSprite(0, 400, 20, 800);
+  rightWall = createSprite(795, 400, 10, 800);
+  topWall = createSprite(400, 0, 800, 20);
+    
+  things = createSprite(mouseX, mouseY, 30, 30);
+  things.velocity.x = random(-5, 5);
+  things.velocity.y = random(-5, 5);
+  things.mouseIsPressed = true
 }
 
 
@@ -23,9 +35,11 @@ function draw() {
   text('Click to create a new sprite, move mouse to hit the wall', width/2, height/2);
 //  fill(255);
     
-    var leftWall = createSprite(0, 400, 20, 800);
-    var rightWall = createSprite(795, 400, 10, 800);
-    var topWall = createSprite(400, 0, 800, 20);
+  things.bounce(leftWall);
+    
+//    var leftWall = createSprite(0, 400, 20, 800);
+//    var rightWall = createSprite(795, 400, 10, 800);
+//    var topWall = createSprite(400, 0, 800, 20);
     
   //draw all the sprites added to the sketch so far
   //the positions will be updated automatically at every cycle
@@ -33,15 +47,15 @@ function draw() {
   
 }
 
-function mousePressed() {
-
-  //create a sprite at the mouse position and store it in a temporary variable
-  var s = createSprite(mouseX, mouseY, 30, 30);
-  //if no image or animation is associated it will be a rectancle of the specified size
-  //and a random color
-
-  //now you can use the variable to set properties
-  //e.g. a random velocity on the x and y coordinates
-  s.velocity.x = random(-5, 5);
-  s.velocity.y = random(-5, 5);
-}
+//function mousePressed() {
+//
+//  //create a sprite at the mouse position and store it in a temporary variable
+//  var s = createSprite(mouseX, mouseY, 30, 30);
+//  //if no image or animation is associated it will be a rectancle of the specified size
+//  //and a random color
+//
+//  //now you can use the variable to set properties
+//  //e.g. a random velocity on the x and y coordinates
+//  s.velocity.x = random(-5, 5);
+//  s.velocity.y = random(-5, 5);
+//}
