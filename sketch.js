a0a005d17d25568d811b606a69a5b9220f71a9f6
victoriesinarow <57379431+victoriesinarow@users.noreply.github.com>
2019-11-18 15:06:42 +0900

    size

--- sketch.js
+++ sketch.js
@@ -54,18 +54,18 @@ function draw() {
   
 }
 
-//function mousePressed() {
-//
-//  //create a sprite at the mouse position and store it in a temporary variable
-//  var s = createSprite(mouseX, mouseY, 30, 30);
-//  //if no image or animation is associated it will be a rectancle of the specified size
-//  //and a random color
-//
-//  //now you can use the variable to set properties
-//  //e.g. a random velocity on the x and y coordinates
-////  s.velocity.x = random(-5, 5);
-////  s.velocity.y = random(-5, 5);
+function mousePressed() {
+
+  //create a sprite at the mouse position and store it in a temporary variable
+  var s = createSprite(mouseX, mouseY, random(), random());
+  //if no image or animation is associated it will be a rectancle of the specified size
+  //and a random color
+
+  //now you can use the variable to set properties
+  //e.g. a random velocity on the x and y coordinates
 //  s.velocity.x = random(-5, 5);
 //  s.velocity.y = random(-5, 5);
-//}
+  s.velocity.x = random(-5, 5);
+  s.velocity.y = random(-5, 5);
+}
 
