e9dfbdf6ce2df0744ead8abc726d186f6bfd5f31
victoriesinarow <57379431+victoriesinarow@users.noreply.github.com>
2019-11-18 16:01:47 +0900

    wall5

--- sketch.js
+++ sketch.js
@@ -5,17 +5,17 @@ let canvasHeight = 400;
 var leftWall;
 var rightWall;
 var topWall;
-var bottomWall;
+
 
 function setup() {
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.position(windowWidth/2 - canvasWidth/2, 20);
   noCursor();
     
-  leftWall = createSprite(0, 400, 20, 800);
-  rightWall = createSprite(795, 400, 10, 800);
-  topWall = createSprite(400, 0, 800, 20);
-  bottomWall = createSprite(0, 400, 20, 800);
+  leftWall = createSprite(0, 400, 40, 800);
+  rightWall = createSprite(795, 400, 30, 800);
+  topWall = createSprite(400, 0, 800, 40);
+
 }
 
 function draw() {
