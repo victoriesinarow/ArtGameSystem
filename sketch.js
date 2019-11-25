f28e9edd13a60c757b42c50e3527002ffd0ea9c6
victoriesinarow <57379431+victoriesinarow@users.noreply.github.com>
2019-11-25 15:43:08 +0900

    bug12

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ function draw() {
   
 
   
-  if (car1.bounce(frog)) {   //개구리가 차를 쳤을 때
+  if (frog.bounce(car1)) {   //개구리가 차를 쳤을 때
 //    assets_hit.play();
   }
   
@@ -60,7 +60,7 @@ function resetGame() {
   goal = createSprite(width/2, 0, width, 4);
   car1 = createSprite(0, height/2, 60, 30);
                       
-  bug1 = createSprite(width/2, height+20, 20, 20);
+  bug1 = createSprite(width/2, height-10, 20, 20);
   
   car1.setVelocity(random(3, 10), 0);
 }
